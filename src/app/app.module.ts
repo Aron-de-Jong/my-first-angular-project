6642fa058f6b310c27a751a1244a5db7398f170e
Aron-de-Jong <122380939+Aron-de-Jong@users.noreply.github.com>
2023-01-17 00:57:44 +0100

    1 file modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { HeroChildComponent } from './hero-child/hero-child.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   imports: [
@@ -20,7 +22,9 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    HeroChildComponent,
+    HeroComponent
   ],
   bootstrap: [
     AppComponent
